780ad6b588fff51685d81761d03426b1817ee286
SergioGM016 <sgmgarcia016@gmail.com>
2017-02-12 12:25:17 +0100

    Add files via upload

---
+++ assets/js/Licencias.js
@@ -0,0 +1,39 @@
+/**
+ *   Autor=Elena-Trabajo
+ *   Fecha=11 de feb. de 2017
+ *   Licencia=GPL v3 
+ *   Version=1.0 (versión del programa)
+ *   Descripcion=Descripción general del contenido de este script. Por ejemplo,
+ *   script para validaciones de entrada de datos del usuario. NIF, números, texto... 
+**/
+
+/* 
+ * Copyright (C) 2017 Elena-Trabajo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Cuando el nombre de la variable no sea representativo de lo que almacena, añade un comentario explicativo.
+//Comentarios explicativos del código siempre que sea necesario
+
+//...
+
+/**
+ * descripción general de lo que hace la función
+ * descripción de argumentos de entrada
+ */
+function cerrarVentana(){
+	window.close();
+	
+}
